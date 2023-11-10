4c742d4b4e8ebf9af511933e835a8e4410be42a4
bujosa <davidbujosa@gmail.com>
2023-11-10 19:08:45 -0400

    feat: implement user controller logic

--- src/main/java/com/bujosa/beid/user/controller/UserController.java
+++ src/main/java/com/bujosa/beid/user/controller/UserController.java
@@ -1,5 +1,50 @@
 package com.bujosa.beid.user.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bujosa.beid.user.dtos.UserDto;
+import com.bujosa.beid.user.entity.User;
+import com.bujosa.beid.user.service.UserService;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
     
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAll() {
+        return ResponseEntity.ok(userService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(String id) {
+        return ResponseEntity.ok(userService.getById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody UserDto userDto) {
+        return ResponseEntity.ok(userService.create(userDto));
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable("id") String id, @RequestBody UserDto userDto) {
+        return ResponseEntity.ok(userService.update(id, userDto));
+    }
+
+    @PostMapping("/{id}/delete")
+    public ResponseEntity<Void> delete(@PathVariable("id") String id) {
+        userService.delete(id);
+        return ResponseEntity.ok().build();
+    }
 }
