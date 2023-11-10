2a16ca1a2c910d1c19e40aad6cdd64314537591e
bujosa <davidbujosa@gmail.com>
2023-11-10 19:01:56 -0400

    feat: implement User constructor with args

--- src/main/java/com/bujosa/beid/user/entity/User.java
+++ src/main/java/com/bujosa/beid/user/entity/User.java
@@ -1,8 +1,9 @@
 package com.bujosa.beid.user.entity;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,11 +11,18 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Document(collection = "users")
 public class User {
+    @Id
+    @Field("_id")
     private String id;
     private String name;
     private String lastName;
     private String email;
+
+    public User(String name, String lastName, String email) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+    }
 }
