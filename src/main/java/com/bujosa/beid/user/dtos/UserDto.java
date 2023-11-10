6902d6b715b5fb22aeb547325ddbccdaaa01aa5b
bujosa <davidbujosa@gmail.com>
2023-11-10 19:01:29 -0400

    feat: implement user dto

---
+++ src/main/java/com/bujosa/beid/user/dtos/UserDto.java
@@ -0,0 +1,22 @@
+package com.bujosa.beid.user.dtos;
+
+import com.bujosa.beid.user.entity.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private String name;
+    private String lastName;
+    private String email;
+
+    public User convertToUser() {
+        return new User(name, lastName, email);
+    }
+}
