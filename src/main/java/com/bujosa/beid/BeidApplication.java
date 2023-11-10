21a5117be4167fe366fc553b94d8f99d69c8ff71
bujosa <davidbujosa@gmail.com>
2023-11-10 17:02:14 -0400

    feat: init spring boot project

---
+++ src/main/java/com/bujosa/beid/BeidApplication.java
@@ -0,0 +1,13 @@
+package com.bujosa.beid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bujosa/beid/BeidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bujosa.beid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
