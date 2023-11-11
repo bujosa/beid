ad9118adc9f1d4ae5f3ded09d3889c69e4f958a3
bujosa <davidbujosa@gmail.com>
2023-11-10 20:06:46 -0400

    feat(common): implement global exception

---
+++ src/main/java/com/bujosa/beid/common/exceptions/GlobalException.java
@@ -0,0 +1,18 @@
+package com.bujosa.beid.common.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.bujosa.beid.common.dto.MessageDto;
+
+@RestControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<MessageDto> notFoundException(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MessageDto(HttpStatus.NOT_FOUND, ex.getMessage()));
+    }
+
+}
