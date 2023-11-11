f0b795c06c34d63ec698470d53036f767613b5b0
bujosa <davidbujosa@gmail.com>
2023-11-10 20:45:36 -0400

    feat(user): add user dto validation

--- src/main/java/com/bujosa/beid/user/dtos/UserDto.java
+++ src/main/java/com/bujosa/beid/user/dtos/UserDto.java
@@ -2,6 +2,9 @@ package com.bujosa.beid.user.dtos;
 
 import com.bujosa.beid.user.entity.User;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,9 +15,15 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
-    // @NotBlank(message = "Name is mandatory")
+    @NotBlank(message = "Name is mandatory")
+    @Min(value = 2, message = "Name should have at least 2 characters")
     private String name;
+
+    @NotBlank(message = "Last name is mandatory")
+    @Min(value = 2, message = "Last name should have at least 2 characters")
     private String lastName;
+
+    @Email(message = "Email should be valid")
     private String email;
 
     public User convertToUser() {
